87c92237087e13d87c56ee03b318bd7c4474435e
Adam Soliev <asoliyev13@gmail.com>
2020-02-19 21:08:33 -0500

    part one - completed

--- get_historical_prices.py
+++ get_historical_prices.py
@@ -2,21 +2,36 @@ import requests
 import matplotlib.pyplot as plt
 import pandas as pd 
 import time
-
+from bs4 import BeautifulSoup 
 
 # define the url
-ticker = 'GOOG'
-url = f"https://api.tdameritrade.com/v1/marketdata/{ticker}/pricehistory"
+ticker_raw = input(f'Please enter ticker symbol: ')
+period = int(input(f'Please enter # of years: '))
+
+ticker = ticker_raw.strip().upper()
+# period = period_raw.strip()
+
+url_for_price = f"https://api.tdameritrade.com/v1/marketdata/{ticker}/pricehistory"
 
 # this part is the control. Change period (years) 
 attributes = {'apikey':'0VUBGJN2ZEGBQWNSWB3AJAGRGULVTHAJ',
             'periodType':'year',
-            'period': '5',
+            'period': f'{period}',
             'frequencyType':'daily',
             'frequency':'1',
             'needExtendedHoursData':'false'}
 
-content = requests.get(url = url, params = attributes)
+content = requests.get(url = url_for_price, params = attributes)
+
+# get the full company name
+url_for_name = f"https://finviz.com/quote.ashx?t={ticker}&ty=c&p=d&b=1"
+response = requests.get(url_for_name).text
+soup = BeautifulSoup(response, 'lxml')
+#Clean the company name
+company_title = soup.title.text.split('.')[0]
+company_title = company_title.split(' ')[1:]
+company_title = ' '.join(company_title)
+
 
 data = content.json()
 
@@ -29,6 +44,7 @@ df['date'] = pd.to_datetime(df['datetime'], unit='ms')
 
 # plot the data to quickly check
 plt.plot(df['date'], df['open'])
+plt.title(f'{company_title} - {period} years.')
 plt.xlabel('date')
 plt.ylabel('price')
 plt.show()
